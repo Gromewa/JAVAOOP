bda157e106e569385cf4a9b411ef8db7e1c7511d
Alexey Baybuzenko <alexeybaybuzenko@gmail.com>
2017-08-17 13:20:14 +0300

    Update AutoMain.java
    Пробел после psvm

--- src/main/java/vertex/lesson1/homework/Automobil/AutoMain.java
+++ src/main/java/vertex/lesson1/homework/Automobil/AutoMain.java
@@ -3,6 +3,7 @@ package vertex.lesson1.homework.Automobil;
 public class AutoMain {
 
         public static void main(String[] args) {
+                
             Car sportsCar = new Car("White","McLaren",2);
             sportsCar.drive();
             sportsCar.stop();
